186700ed7efb13016eb05ca3aa1b5a3b7a7a3493
Iris <i5052637@gmail.com>
2023-10-10 16:06:32 -0400

    add auth detail:

--- dns_app/FS/run.py
+++ dns_app/FS/run.py
@@ -1,7 +1,19 @@
 from flask import Flask, make_response, request
+import json
+import socket
 
 app = Flask(__name__)
-body_info = {}
+UDP_IP = "0.0.0.0"
+UDP_PORT = 53533
+
+
+def register_AS(hostname, ip, as_ip, as_port):
+    body = {'NAME': hostname, 'VALUE': ip, 'TYPE': 'A', 'TTL': 10}
+    message = json.dumps(body).encode('utf-8')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message, (UDP_IP, UDP_PORT))
+
+    return 'Created', 201
 
 
 @app.route("/register", methods=['PUT'])
@@ -11,24 +23,29 @@ def registerFib():
     ip = data.get('ip')
     as_ip = data.get('as_ip')
     as_port = data.get('as_port')
+
     if not all([hostname, ip, as_ip, as_port]):
         return 'Bad Request', 400
-    body_info = {'ip': ip, 'as_ip': as_ip, 'as_port': as_port}
 
-    return 'Created', 201
+    return register_AS(hostname, ip, as_ip, as_port)
+
+
+def fibonacci(x):
+    if x == 1 or x == 0:
+        return x
+    return fibonacci(x - 1) + fibonacci(x - 2)
 
 
 @app.route('/fibonacci', methods=['GET'])
 def get_fibonacci():
     number = request.args.get('number')
-    fib_ans = 0
 
     try:
-        number = int(number)
+        x = int(number)
     except ValueError:
         return 'Bad Format', 400
 
-    return fib_ans
+    return fibonacci(x), 200
 
 
 app.run(host='0.0.0.0',
--- dns_app/US/run.py
+++ dns_app/US/run.py
@@ -25,20 +25,3 @@ def fibonacci():
 app.run(host='0.0.0.0',
         port=8080,
         debug=True)
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 53533
-MESSAGE = b"Hello, World!"
-
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
-
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
-TYPE = A
-NAME = fibonacci.com
-VALUE = IP_ADDRESS
-TTL = 10
